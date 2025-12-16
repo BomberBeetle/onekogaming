493267314c1609421a87b8cf82f4c97c01d556f0
BomberBeetle <gdreks@gmail.com>
2025-12-15 23:45:15 -0300

    add wall and cheese oob check

--- chease.gd
+++ chease.gd
@@ -8,6 +8,11 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	global_position += Vector2(0.01, 0.01)
+	var glx = global_position.x
+	var gly = global_position.y
+	var oob = (glx >= %end.global_position.x) or (glx <= %start.global_position.x) or (gly >= %end.global_position.y) or (gly <= %start.global_position.y)
+	if oob:
+		teleport()
 	
 func teleport():
 	global_position = Vector2(randi_range(%start.global_position.x, %end.global_position.x), randi_range(%start.global_position.y, %end.global_position.y))
